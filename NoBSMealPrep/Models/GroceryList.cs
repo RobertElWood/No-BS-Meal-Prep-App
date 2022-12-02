525fa90c8024a1c0eb545aad3b3dddadf5fa7a40
Robert Woods <robert.woods@fulbrightmail.org>
2022-12-02 11:05:09 -0500

    Added DB.txt file, scaffolded databases and controllers

---
+++ NoBSMealPrep/Models/GroceryList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NoBSMealPrep.Models;
+
+public partial class GroceryList
+{
+    public int Id { get; set; }
+
+    public string? Food { get; set; }
+
+    public double? Quantity { get; set; }
+
+    public string? Measure { get; set; }
+
+    public int? ParentRecipe { get; set; }
+
+    public virtual FavoriteRecipe? ParentRecipeNavigation { get; set; }
+}
---
+++ NoBSMealPrep/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NoBSMealPrep.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? Logininfo { get; set; }
+
+    public virtual ICollection<FavoriteRecipe> FavoriteRecipes { get; } = new List<FavoriteRecipe>();
+}
